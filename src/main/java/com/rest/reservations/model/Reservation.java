eba0b1033899a382d84e125db4b12c0632661c3f
Annette Mavec <admin@ADMINs-MacBook-Pro.local>
2022-02-01 12:57:52 -0600

    added reservation column properties that are in ERD

--- src/main/java/com/rest/reservations/model/Reservation.java
+++ src/main/java/com/rest/reservations/model/Reservation.java
@@ -1,5 +1,26 @@
 package com.rest.reservations.model;
 
 public class Reservation {
-    
-}
+    @reservation_Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Integer table_Id;
+
+    @Column
+    private Integer customer_Id;
+
+    @Column
+    private String date;
+
+    @Column
+    private String time;
+
+    @Column
+    private Integer party_of;
+
+    @Column
+    private Integer customer_Id;
+
