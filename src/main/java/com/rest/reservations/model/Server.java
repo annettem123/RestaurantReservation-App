9ff501edb96d7acdf24ac6d0ed03c57575493425
Annette Mavec <admin@ADMINs-MacBook-Pro.local>
2022-02-01 14:49:46 -0600

    added server column properties that are in ERD

--- src/main/java/com/rest/reservations/model/Server.java
+++ src/main/java/com/rest/reservations/model/Server.java
@@ -1,4 +1,17 @@
 package com.rest.reservations.model;
 
 public class Server {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String first_name;
+
+    @Column
+    private String last_name;
+
+    @Column
+    private Integer table_Id;
 }
