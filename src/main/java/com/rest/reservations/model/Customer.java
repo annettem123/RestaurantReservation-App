3fb2bf0b73e6b2ee2ddfbb0981610ff689168cf8
Annette Mavec <admin@ADMINs-MacBook-Pro.local>
2022-02-01 14:30:57 -0600

    added customer model getters and setters

--- src/main/java/com/rest/reservations/model/Customer.java
+++ src/main/java/com/rest/reservations/model/Customer.java
@@ -20,4 +20,52 @@ public class Customer {
 
     @Column
     private Integer reservation_Id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getMobile_number() {
+        return mobile_number;
+    }
+
+    public void setMobile_number(String mobile_number) {
+        this.mobile_number = mobile_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getReservation_Id() {
+        return reservation_Id;
+    }
+
+    public void setReservation_Id(Integer reservation_Id) {
+        this.reservation_Id = reservation_Id;
+    }
 }
