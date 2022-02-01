dad80a916ff131c6efafe5fc9e4db4e10b137091
Annette Mavec <admin@ADMINs-MacBook-Pro.local>
2022-02-01 13:55:45 -0600

    added reservation model getters and setters

--- src/main/java/com/rest/reservations/model/Reservation.java
+++ src/main/java/com/rest/reservations/model/Reservation.java
@@ -1,7 +1,7 @@
 package com.rest.reservations.model;
 
 public class Reservation {
-    @reservation_Id
+    @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,4 +23,53 @@ public class Reservation {
 
     @Column
     private Integer customer_Id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getTable_Id() {
+        return table_Id;
+    }
+
+    public void setTable_Id(Integer table_Id) {
+        this.table_Id = table_Id;
+    }
+
+    public Integer getCustomer_Id(Integer customer_Id) {
+        return customer_Id;
+    }
+
+    public void setCustomer_Id(Integer customer_Id) {
+        this.customer_Id = customer_Id;
+    }
+
+    public String getDate(String date){
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime(String time) {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Integer getParty_of(Integer party_of) {
+        return party_of;
+    }
+
+    public void setParty_of(Integer party_of) {
+        this.party_of = party_of;
+    }
+
 
