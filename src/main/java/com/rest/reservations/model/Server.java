b508791c655079341a80cebe0eafe0bd750b6e49
Annette Mavec <admin@ADMINs-MacBook-Pro.local>
2022-02-01 14:50:50 -0600

    added server model getters and setters

--- src/main/java/com/rest/reservations/model/Server.java
+++ src/main/java/com/rest/reservations/model/Server.java
@@ -14,4 +14,36 @@ public class Server {
 
     @Column
     private Integer table_Id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Integer getTable_Id() {
+        return table_Id;
+    }
+
+    public void setTable_Id(Integer table_Id) {
+        this.table_Id = table_Id;
+    }
 }
