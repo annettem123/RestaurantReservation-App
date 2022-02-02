a7970b48d8ca97cca23954309bf04c2d3b9bca62
Annette Mavec <admin@ADMINs-MacBook-Pro.local>
2022-02-02 16:29:24 -0600

    Added code to CustomerRepository class

--- src/main/java/com/rest/reservations/repository/CustomerRepository.java
+++ src/main/java/com/rest/reservations/repository/CustomerRepository.java
@@ -1,4 +1,11 @@
 package com.rest.reservations.repository;
 
-public class CustomerRepository {
+import com.rest.reservations.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findByName(String CustomerName);
+
 }
